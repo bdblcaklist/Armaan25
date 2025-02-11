e5bc48b08012998565e79d553831bff529f457c2
bdblacklist <motiurny@gmail.com>
2025-02-11 18:54:29 -0500

    new project testing for push pull

---
+++ src/basic/DoWhileLoopPractice.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class DoWhileLoopPractice {
+	
+	
+	public static void main(String [] args ) {
+		
+		
+		int i = 1; // You have to initilize Value 
+		
+		while (i<=10) { // then you must show the condition
+			
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+			//System.out.println(i);
+			i++;
+		}
+	}
+
+}
---
+++ src/basic/EnhanchedForLoop.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class EnhanchedForLoop {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
